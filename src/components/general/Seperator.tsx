f35b91653ad6cb26c4d88b09d35faac6d83fa1f1
Balazs <121605136+Balazs-topg@users.noreply.github.com>
2024-01-03 16:31:35 +0100

    Clear git cache

--- src/components/general/Seperator.tsx
+++
@@ -1,5 +0,0 @@
-import Container from "./Container";
-
-export default function Seperator() {
-  return <Container className="border-t border-dashed border-white/50 py-0" />;
-}
--- src/components/layout/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import Container from "../general/Container";
-
-export default function Footer() {
-  return (
-    <footer className=" border-t border-dashed border-white/50">
-      <Container>Hemsida skapad av Balazs Hevesi 2023</Container>
-    </footer>
-  );
-}
